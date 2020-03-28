46b44090d3cb35345d674e6335ea2b5b2b948d40
Liopin <contact.pinheiro.lionel@gmail.com>
2020-03-28 15:15:52 +0100

    gatsbt link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+
+export default () => (
+    <>
+        <h1>Home</h1>
+        <p>Hello Frontend Masters!</p>
+        <Link to="/about/">Learn about me &rarr;</Link>          
+    </>
+);
